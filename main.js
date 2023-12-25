6a814e059410e279701ec3ca4e4eb36213585a50
Nantawut101 <136212250+nantawoot101@users.noreply.github.com>
2023-12-25 19:10:48 +0700

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,6 @@ function Counter() {
 function App(){
     return (
         <div className="counter1">
-            <h1>Today : {new Date().toDateString()}</h1>
             <h1>Sum = 0</h1>
             <button className="btn" onClick={hlClick}>Add Counter</button>
             <Counter />
